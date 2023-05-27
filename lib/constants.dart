3069274a573ed62bbd6dfcd65b636f32deee9ccc
Aditya Rawat <adityarawat@gmail.com>
2023-05-28 00:52:39 +0530

    Minor tweak to final project

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kMainDarkColor= Color(0xFFFEEFEC);
-const kMainColor= Color(0xFF9DE47C);
+const kMainDarkColor= Color(0xFFB3B2B7);
+const kMainColor= Color(0xFF505050);
